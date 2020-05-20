522067c5ab91df344c008c2233f985ee59e6c113
Yaacov Rydzinski <yaacovCR@gmail.com>
2020-05-20 13:17:45 -0400

    workaround

--- src/test.js
+++ src/test.js
@@ -2,6 +2,7 @@ const { createPostGraphileSchema, withPostGraphileContext } = require('postgraph
 const { Pool } = require('pg');
 const { graphql } = require ('graphql');
 const { mapSchema } = require('@graphql-tools/utils');
+const { wrapSchema } = require('@graphql-tools/wrap');
 
 describe('postgraphile', () => {
   test('mapping postgraphile schema works', async () => {
@@ -11,6 +12,45 @@ describe('postgraphile', () => {
       connectionString,
       'public'
     );
+
+    const pgPool = new Pool({
+      connectionString
+    });
+
+    const newSchema = mapSchema(schema, {});
+
+    const result = await withPostGraphileContext(
+      {
+        pgPool,
+      },
+      async (context) => await graphql(
+        newSchema,
+        `
+          query locations {
+            allCities(orderBy: ID_ASC, first: 10) {
+              nodes {
+                name
+              }
+            }
+          }
+        `,
+        null,
+        context,
+      )
+    );
+
+    await pgPool.end();
+
+    expect(result.errors).toBeUndefined();
+  });
+
+  test('mapping wrapped postgraphile schema works', async () => {
+    const connectionString = 'postgres://postgres:docker@localhost:5432/postgres';
+
+    const schema = wrapSchema(await createPostGraphileSchema(
+      connectionString,
+      'public'
+    ));
 
     const pgPool = new Pool({
       connectionString
